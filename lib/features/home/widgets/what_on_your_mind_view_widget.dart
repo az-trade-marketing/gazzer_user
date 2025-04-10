aabd0dc9fe57a7c75a98a6ee300718732f6af6f2
tarek <tariqkk554@gmail.com>
2025-04-10 05:36:42 +0200

    Switch restaurants from list view to grid view

--- lib/features/home/widgets/what_on_your_mind_view_widget.dart
+++ lib/features/home/widgets/what_on_your_mind_view_widget.dart
@@ -36,151 +36,128 @@ class WhatOnYourMindViewWidget extends StatelessWidget {
           ),
           child: ResponsiveHelper.isDesktop(context)
               ? Text('what_on_your_mind'.tr,
-                  style: robotoMedium.copyWith(
-                      fontSize: Dimensions.fontSizeLarge,
-                      fontWeight: FontWeight.w600))
+              style: robotoMedium.copyWith(
+                  fontSize: Dimensions.fontSizeLarge,
+                  fontWeight: FontWeight.w600))
               : Padding(
-                  padding: const EdgeInsets.only(
-                      left: Dimensions.paddingSizeSmall,
-                      right: Dimensions.paddingSizeDefault),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('what_on_your_mind'.tr,
-                          style: robotoBold.copyWith(
-                              fontSize: Dimensions.fontSizeLarge,
-                              fontWeight: FontWeight.w600)),
-                      ArrowIconButtonWidget(
-                          onTap: () =>
-                              Get.toNamed(RouteHelper.getCategoryRoute())),
-                    ],
+            padding: const EdgeInsets.only(
+                left: Dimensions.paddingSizeSmall,
+                right: Dimensions.paddingSizeDefault),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('what_on_your_mind'.tr,
+                    style: robotoBold.copyWith(
+                        fontSize: Dimensions.fontSizeLarge,
+                        fontWeight: FontWeight.w600)),
+                ArrowIconButtonWidget(
+                    onTap: () =>
+                        Get.toNamed(RouteHelper.getCategoryRoute())),
+              ],
+            ),
+          ),
+        ),
+        categoryController.categoryList != null
+            ? GridView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            childAspectRatio: ResponsiveHelper.isMobile(context) ? 0.7 : 0.85,
+            crossAxisSpacing: Dimensions.paddingSizeSmall,
+            mainAxisSpacing: Dimensions.paddingSizeSmall,
+          ),
+          padding: const EdgeInsets.symmetric(
+              horizontal: Dimensions.paddingSizeDefault),
+          itemCount: categoryController.categoryList!.length > 16
+              ? 16
+              : categoryController.categoryList!.length,
+          itemBuilder: (context, index) {
+            if (index == 15 && categoryController.categoryList!.length > 16) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                  borderRadius: BorderRadius.circular(Dimensions.radiusSmall),
+                  border: Border.all(
+                      color: Theme.of(context).primaryColor.withOpacity(0.3)),
+                ),
+                child: CustomInkWellWidget(
+                  onTap: () => Get.toNamed(RouteHelper.getCategoryRoute()),
+                  radius: Dimensions.radiusSmall,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.arrow_forward,
+                            color: Theme.of(context).primaryColor, size: 30),
+                        const SizedBox(height: Dimensions.paddingSizeSmall),
+                        Text(
+                          'view_all'.tr,
+                          style: robotoMedium.copyWith(
+                            fontSize: Dimensions.fontSizeSmall,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-        ),
-        SizedBox(
-          height: ResponsiveHelper.isMobile(context) ? 120 : 170,
-          child: categoryController.categoryList != null
-              ? ListView.builder(
-                  physics: ResponsiveHelper.isMobile(context)
-                      ? const BouncingScrollPhysics()
-                      : const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.only(
-                      left: Dimensions.paddingSizeDefault),
-                  itemCount: categoryController.categoryList!.length > 10
-                      ? 10
-                      : categoryController.categoryList!.length,
-                  itemBuilder: (context, index) {
-                    if (index == 9) {
-                      return ResponsiveHelper.isDesktop(context)
-                          ? Padding(
-                              padding: const EdgeInsets.only(
-                                  bottom: Dimensions.paddingSizeSmall,
-                                  top: Dimensions.paddingSizeSmall),
-                              child: Container(
-                                width: 70,
-                                padding: const EdgeInsets.only(
-                                    left: Dimensions.paddingSizeExtraSmall,
-                                    right: Dimensions.paddingSizeExtraSmall,
-                                    top: Dimensions.paddingSizeSmall,
-                                    bottom: Dimensions.paddingSizeSmall),
-                                child: Align(
-                                  alignment: Alignment.centerRight,
-                                  child: InkWell(
-                                    hoverColor: Colors.transparent,
-                                    onTap: () => Get.toNamed(
-                                        RouteHelper.getCategoryRoute()),
-                                    child: Container(
-                                      height: 40,
-                                      width: 40,
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: Theme.of(context).cardColor,
-                                        border: Border.all(
-                                            color: Theme.of(context)
-                                                .primaryColor
-                                                .withOpacity(0.3)),
-                                      ),
-                                      child: Icon(Icons.arrow_forward,
-                                          color:
-                                              Theme.of(context).primaryColor),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            )
-                          : const SizedBox();
-                    }
+              );
+            }
 
-                    return Padding(
-                      padding: const EdgeInsets.only(
-                          bottom: Dimensions.paddingSizeSmall,
-                          right: Dimensions.paddingSizeDefault),
-                      child: Container(
-                        width: ResponsiveHelper.isMobile(context) ? 70 : 100,
-                        height: ResponsiveHelper.isMobile(context) ? 70 : 100,
+            return Container(
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(Dimensions.radiusSmall),
+              ),
+              child: CustomInkWellWidget(
+                onTap: () =>
+                    Get.toNamed(RouteHelper.getCategoryProductRoute(
+                      categoryController.categoryList![index].id,
+                      categoryController.categoryList![index].name!,
+                    )),
+                radius: Dimensions.radiusSmall,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(2),
                         decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          borderRadius:
-                              BorderRadius.circular(Dimensions.radiusSmall),
+                          borderRadius: BorderRadius.circular(Dimensions.radiusDefault),
+                          color: Theme.of(context).disabledColor.withOpacity(0.2),
                         ),
-                        child: CustomInkWellWidget(
-                          onTap: () =>
-                              Get.toNamed(RouteHelper.getCategoryProductRoute(
-                            categoryController.categoryList![index].id,
-                            categoryController.categoryList![index].name!,
-                          )),
-                          radius: Dimensions.radiusSmall,
-                          child: Column(children: [
-                            Container(
-                              padding: const EdgeInsets.all(2),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(
-                                    Dimensions.radiusDefault),
-                                color: Theme.of(context)
-                                    .disabledColor
-                                    .withOpacity(0.2),
-                              ),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(
-                                    Dimensions.radiusDefault),
-                                child: CustomImageWidget(
-                                  image:
-                                      '${Get.find<SplashController>().configModel!.baseUrls!.categoryImageUrl}/${categoryController.categoryList![index].image}',
-                                  height: ResponsiveHelper.isMobile(context)
-                                      ? 70
-                                      : 100,
-                                  width: ResponsiveHelper.isMobile(context)
-                                      ? 70
-                                      : 100,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                                height: ResponsiveHelper.isMobile(context)
-                                    ? Dimensions.paddingSizeDefault
-                                    : Dimensions.paddingSizeLarge),
-                            Expanded(
-                                child: Text(
-                              categoryController.categoryList![index].name!,
-                              style: robotoMedium.copyWith(
-                                fontSize: Dimensions.fontSizeSmall,
-                              ),
-                              maxLines: 3,
-                              overflow: TextOverflow.visible,
-                              textAlign: TextAlign.center,
-                            )),
-                          ]),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(Dimensions.radiusDefault),
+                          child: CustomImageWidget(
+                            image: '${Get.find<SplashController>().configModel!.baseUrls!.categoryImageUrl}/${categoryController.categoryList![index].image}',
+                            height: ResponsiveHelper.isMobile(context) ? 60 : 80,
+                            width: ResponsiveHelper.isMobile(context) ? 60 : 80,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    );
-                  },
-                )
-              : WebWhatOnYourMindViewShimmer(
-                  categoryController: categoryController),
-        ),
+                      SizedBox(height: Dimensions.paddingSizeSmall),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: Dimensions.paddingSizeExtraSmall),
+                          child: Text(
+                            categoryController.categoryList![index].name!,
+                            style: robotoMedium.copyWith(
+                              fontSize: Dimensions.fontSizeSmall,
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+            );
+          },
+        )
+            : WebWhatOnYourMindViewShimmer(categoryController: categoryController),
         const SizedBox(height: Dimensions.paddingSizeLarge),
       ]);
     });
@@ -195,58 +172,48 @@ class WebWhatOnYourMindViewShimmer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: ResponsiveHelper.isMobile(context) ? 120 : 170,
-      child: ListView.builder(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: Dimensions.paddingSizeDefault),
+      child: GridView.builder(
         physics: const NeverScrollableScrollPhysics(),
         shrinkWrap: true,
-        itemCount: 10,
-        scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.only(left: Dimensions.paddingSizeSmall),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          childAspectRatio: ResponsiveHelper.isMobile(context) ? 0.7 : 0.85,
+          crossAxisSpacing: Dimensions.paddingSizeSmall,
+          mainAxisSpacing: Dimensions.paddingSizeSmall,
+        ),
+        itemCount: 12,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(
-                bottom: Dimensions.paddingSizeSmall,
-                right: Dimensions.paddingSizeSmall,
-                top: Dimensions.paddingSizeSmall),
-            child: Container(
-              width: ResponsiveHelper.isMobile(context) ? 70 : 108,
-              height: ResponsiveHelper.isMobile(context) ? 70 : 100,
-              padding: const EdgeInsets.all(Dimensions.paddingSizeExtraSmall),
-              margin: EdgeInsets.only(
-                  top: ResponsiveHelper.isMobile(context)
-                      ? 0
-                      : Dimensions.paddingSizeSmall),
-              child: Column(children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(Dimensions.radiusSmall),
-                  child: Shimmer(
-                    child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(Dimensions.radiusSmall),
-                          color: Theme.of(context).shadowColor),
-                      height: ResponsiveHelper.isMobile(context) ? 70 : 80,
-                      width: 70,
-                    ),
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(Dimensions.radiusSmall),
+                child: Shimmer(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Dimensions.radiusSmall),
+                        color: Theme.of(context).shadowColor),
+                    height: ResponsiveHelper.isMobile(context) ? 60 : 80,
+                    width: ResponsiveHelper.isMobile(context) ? 60 : 80,
                   ),
                 ),
-                const SizedBox(height: Dimensions.paddingSizeSmall),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(Dimensions.radiusSmall),
-                  child: Shimmer(
-                    child: Container(
-                      height: ResponsiveHelper.isMobile(context) ? 10 : 15,
-                      width: 150,
-                      decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(Dimensions.radiusSmall),
-                          color: Theme.of(context).shadowColor),
-                    ),
+              ),
+              const SizedBox(height: Dimensions.paddingSizeSmall),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(Dimensions.radiusSmall),
+                child: Shimmer(
+                  child: Container(
+                    height: ResponsiveHelper.isMobile(context) ? 10 : 15,
+                    width: 70,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Dimensions.radiusSmall),
+                        color: Theme.of(context).shadowColor),
                   ),
                 ),
-              ]),
-            ),
+              ),
+            ],
           );
         },
       ),

1d355123e24fc5b6edbc888c6d5d67f5964e555a
mario237 <mariomamdouh237@gamil.com>
2025-04-15 00:50:42 +0200

    Change URL Staging

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -8,6 +8,9 @@ class AppConstants {
 
   static const String fontFamily = 'Roboto';
   static const bool payInWevView = false;
+  // Production API URL
+  // static const String webHostedUrl = 'https://dashboard.gazzerapp.com';
+  // Staging API URL
   static const String webHostedUrl = 'https://carrot.refilex.com';
   static const String baseUrl = webHostedUrl;
   static const String categoryUri = '/api/v1/categories';
